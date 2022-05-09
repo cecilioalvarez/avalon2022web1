ae42f5817edd061e1fb7c028d77b6e94173e113a
cecilioAlvarez <cmac@arquitecturajava.com>
2022-05-09 14:43:28 +0200

    ultimos cambios active record

--- src/main/java/es/avalon/activerecord/DataBaseHelper.java
+++ src/main/java/es/avalon/activerecord/DataBaseHelper.java
@@ -2,6 +2,7 @@ package es.avalon.activerecord;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,7 +13,8 @@ public class DataBaseHelper {
     public static Connection getConexion() throws SQLException, IOException {
 
         Properties propiedades = new Properties();
-        propiedades.load(new FileReader("database.properties"));
+        InputStream in= DataBaseHelper.class.getClassLoader().getResourceAsStream("database.properties");
+        propiedades.load(in);
         return DriverManager.getConnection(propiedades.getProperty("url"), propiedades.getProperty("usuario"), propiedades.getProperty("clave"));
 
     }
