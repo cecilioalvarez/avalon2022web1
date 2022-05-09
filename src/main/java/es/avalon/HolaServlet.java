70691a326783927dc678d75f9dba4366b5a29fb0
cecilioAlvarez <cmac@arquitecturajava.com>
2022-05-09 14:15:06 +0200

    integrar active record

--- src/main/java/es/avalon/HolaServlet.java
+++ src/main/java/es/avalon/HolaServlet.java
@@ -2,10 +2,13 @@ package es.avalon;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import es.avalon.activerecord.PersonaActiveRecord;
 
 public class HolaServlet extends HttpServlet {
     
@@ -13,9 +16,16 @@ public class HolaServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
             PrintWriter pw= response.getWriter();
-            for (int i=0;i<100;i++) {
-                pw.println("<p>hola</p>");
+            List<PersonaActiveRecord> lista=PersonaActiveRecord.buscarTodos();
+            for (PersonaActiveRecord persona:lista) {
+
+                pw.println("<p>"+persona.getDni()+"</p>");
+                pw.println("<p>"+persona.getNombre()+"</p>");
+                pw.println("<p>"+persona.getEdad()+"</p>");
+              
             }
+         
+
 
         
     }
