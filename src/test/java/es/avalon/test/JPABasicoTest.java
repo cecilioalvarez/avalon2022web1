e4aaa8ba4b0365c1c42bfa87fb61f1af13541372
cecilioAlvarez <cmac@arquitecturajava.com>
2022-05-13 17:24:33 +0200

    nuevos

--- src/test/java/es/avalon/test/JPABasicoTest.java
+++ src/test/java/es/avalon/test/JPABasicoTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.junit.Test;
@@ -13,7 +14,7 @@ import es.avalon.repository.PersonaRepository;
 import es.avalon.repository.helper.JPAHelper;
 
 public class JPABasicoTest {
-
+    
     @Test
     public void testPersona() {
 
@@ -41,17 +42,101 @@ public class JPABasicoTest {
     public void testJPAConsultaSeleccionarNombre() {
 
         EntityManager em= JPAHelper.getEntityManager();
-        TypedQuery<Persona> consulta=em.createQuery("select p from Persona p where p.nombre=:nombre",Persona.class);
+        TypedQuery<Persona> consulta=em.
+        createQuery("select p from Persona p where p.nombre=:nombre",Persona.class);
         consulta.setParameter("nombre", "maria");
         Persona maria= consulta.getResultList().get(0);
+       
         assertEquals("maria", maria.getNombre());
         assertEquals("2", maria.getDni());
         assertEquals(40, maria.getEdad());
-        
-
-
-
-        
+                
     }
     
+    @Test
+    public void testJPAConsultaSeleccionarNombreAndEdad() {
+
+        EntityManager em= JPAHelper.getEntityManager();
+        TypedQuery<Persona> consulta=em.
+        createQuery("select p from Persona p where"+
+          " p.nombre=:nombre and p.edad=:edad",Persona.class);
+        consulta.setParameter("nombre", "maria");
+        consulta.setParameter("edad", 40);
+        Persona maria= consulta.getSingleResult();
+       
+        assertEquals("maria", maria.getNombre());
+        assertEquals("2", maria.getDni());
+        assertEquals(40, maria.getEdad());
+                
+    }
+
+    @Test
+    public void testJPAConsultaRangoPersonas() {
+
+        EntityManager em= JPAHelper.getEntityManager();
+        TypedQuery<Persona> consulta=em.
+        createQuery("select p from Persona p where"+
+          " p.edad between 10 and 50 ",Persona.class);
+        assertEquals(3,consulta.getResultList().size());
+       
+      
+                
+    }
+
+     
+    @Test
+    public void testJPAEdadPersona() {
+
+        EntityManager em= JPAHelper.getEntityManager();
+        TypedQuery<Integer> consulta=em.
+        createQuery("select p.edad from Persona p where p.nombre='maria' ",Integer.class);
+        Integer numero=consulta.getSingleResult();
+        assertEquals(40, numero.intValue());
+       
+    
+                
+    }
+
+    @Test
+    public void testJPANombrePersonaMayusculas() {
+
+        EntityManager em= JPAHelper.getEntityManager();
+        TypedQuery<String> consulta=em.
+        createQuery("select upper(p.nombre) from Persona p where p.edad=40 ",String.class);
+        String nombre=consulta.getSingleResult();
+        assertEquals("MARIA", nombre);
+       
+    
+                
+    }
+   
+    @Test
+    public void testJPAEdadSumaPersona() {
+
+        EntityManager em= JPAHelper.getEntityManager();
+        TypedQuery<Long> consulta=em.
+        createQuery("select sum(p.edad) from Persona p ",Long.class);
+        Long numero=consulta.getSingleResult();
+        assertEquals(110, numero.intValue());
+       
+    
+                
+    }
+
+
+    @Test
+    public void testJPAORdenadoPorEdad() {
+
+        EntityManager em= JPAHelper.getEntityManager();
+        TypedQuery<Persona> consulta=em.
+        createQuery("select p from Persona p order by p.edad",Persona.class);
+        List<Persona> lista= consulta.getResultList();
+
+        assertEquals("juan", lista.get(0).getNombre());
+        assertEquals("maria", lista.get(1).getNombre());
+       
+    
+                
+    }
+ 
 }
