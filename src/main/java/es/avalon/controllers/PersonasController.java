acbe76eeef4b71abbd6045cf3c765a0c005b246c
cecilioAlvarez <cmac@arquitecturajava.com>
2022-05-18 18:46:50 +0200

    spring thymeleaf

--- src/main/java/es/avalon/controllers/PersonasController.java
+++ src/main/java/es/avalon/controllers/PersonasController.java
@@ -9,13 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import es.avalon.services.LibroPersonaService;
 
 @Controller
+@RequestMapping("/personas")
 public class PersonasController {
 
     @Autowired
     LibroPersonaService servicio;   
     @GetMapping("/lista")
     public String lista(Model modelo) {
-
+        // pasa a la vista el listado de personas con sus libros
+        modelo.addAttribute("lista", servicio.buscarTodasLasPersonas());
         return "lista";
     }
     
